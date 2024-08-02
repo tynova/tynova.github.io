de3fb7699218cca60058f861c9be490c7e152260
tynova <ty.nova87@gmail.com>
2024-08-02 16:10:02 -0400

    Site setup and config

--- portfolio/components/header/header.js
+++ portfolio/components/header/header.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add JavaScript functionality here if needed
+    const pageTitle = '[title]'
+    //Pages
+    const homePage = 'Ty Nova: Portfolio - Home';
+    const contactPage = 'Ty Nova: Portfolio - Contact Me';
+    const projectsPage = 'Ty Nova: Portfolio - Projects';
+    //Get page title
+    let urlPath = toString(window.location.pathname);
+    if (urlPath.includes('contact')) {
+        pageTitle = contactPage;
+    } else if (urlPath.includes('projects')) {
+        pageTitle = projectsPage;
+    } else {pageTitle = homePage;}
 });
