ed0a75ecd4344160761d25ff9daa5b7055b1fcce
tynova <ty.nova87@gmail.com>
2024-08-03 17:00:47 -0400

    Header updates

--- portfolio/components/header/header.js
+++ portfolio/components/header/header.js
@@ -6,12 +6,18 @@ const contactPage = 'Ty Nova: Portfolio - Contact Me';
 const projectsPage = 'Ty Nova: Portfolio - Projects';
 const logoImage = logoImage;
 
-document.addEventListener('DOMContentLoaded', function() {
-    //Assign page title based on url
-    let urlPath = toString(window.location.pathname);
-    if (urlPath.includes('contact')) {
-        pageTitle = contactPage;
-    } else if (urlPath.includes('projects')) {
-        pageTitle = projectsPage;
-    } else {pageTitle = homePage;}
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     //Assign page title based on url
+//     let urlPath = toString(window.location.pathname);
+//     this.getPageTitle(urlPath);
+
+// });
+
+// getPageTitle(urlPath) {
+//     if (urlPath.includes('contact')) {
+//         pageTitle = contactPage;
+//     } else if (urlPath.includes('projects')) {
+//         pageTitle = projectsPage;
+//     } else {pageTitle = homePage;}
+//     return pageTitle;  
+// }
