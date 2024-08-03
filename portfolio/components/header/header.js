197438f1666b8004850561fddf79799613806dea
tynova <ty.nova87@gmail.com>
2024-08-03 17:13:47 -0400

    Header updates

--- portfolio/components/header/header.js
+++ portfolio/components/header/header.js
@@ -5,19 +5,3 @@ const homePage = 'Ty Nova: Portfolio - Home';
 const contactPage = 'Ty Nova: Portfolio - Contact Me';
 const projectsPage = 'Ty Nova: Portfolio - Projects';
 const logoImage = logoImage;
-
-document.addEventListener('DOMContentLoaded', function() {
-    //Assign page title based on url
-    // let urlPath = toString(window.location.pathname);
-    // this.getPageTitle(urlPath);
-
-});
-
-// getPageTitle(urlPath) {
-//     if (urlPath.includes('contact')) {
-//         pageTitle = contactPage;
-//     } else if (urlPath.includes('projects')) {
-//         pageTitle = projectsPage;
-//     } else {pageTitle = homePage;}
-//     return pageTitle;  
-}
