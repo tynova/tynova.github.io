6521f5d319546aade13c4ed0b719572cd80428c8
tynova <ty.nova87@gmail.com>
2024-08-04 16:13:06 -0400

    Header updates

--- portfolio/components/header/header.js
+++ portfolio/components/header/header.js
@@ -1,25 +1,4 @@
-import catImage from '/portfolio/assets/cat.png';
-
-const LABEL_HOME = 'Ty Nova: Portfolio - Home';
-const LABEL_ABOUT = 'Ty Nova: Portfolio - About Me'
-const LABEL_CONTACT = 'Ty Nova: Portfolio - Contact Me';
-const LABEL_PROJECTS = 'Ty Nova: Portfolio - Projects';
-
 class header {
     constructor() {
-        let pageTitle = this.getPageTitle();
-    }
-    getPageTitle() {
-        let pageUrl = window.location.href;
-        if (pageUrl.includes("about")) {
-            this.pageTitle = LABEL_ABOUT;
-        } else if (pageUrl.includes("contact")) {
-            this.pageTitle = LABEL_CONTACT;
-        } else if (pageUrl.includes("projects")) {
-            this.pageTitle = LABEL_PROJECTS;
-        } else { this.pageTitle = LABEL_HOME;}
-    }
-    getLogo() {
-        return catImage;
     }
 }
